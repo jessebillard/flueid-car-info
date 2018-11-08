9ec505aac1a01eeda0c8532a235f999c605e3a73
jessebillard <jessebillard@mac.com>
2018-11-08 12:02:25 -0500

    builds and styles header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='grey-box' />            
+            <div className='black-box' />
+            <h1 className='logo-title'>Company Logo</h1>
+            <p className='date'>Thursday, November 7th, 2018</p>
+        </div>
+    )
+};
+
+export default Header;
